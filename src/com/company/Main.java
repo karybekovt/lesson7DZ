8b877a8b7dac5d14b7b369c112cc646c1177ecf1
karybekovt <tima.chapi@gmail.com>
2020-03-14 18:05:45 +0600

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        HavingSuperAbility[] heroes = {new Magic(), new Warrior(), new Medic()};
+        for (int i = 0; i < heroes.length; i++) {
+            heroes[i].applySuperAbility(": ");
+        }
+    }
+}
